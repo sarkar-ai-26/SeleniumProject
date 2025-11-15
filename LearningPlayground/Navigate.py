698cbfd08ee8c6ab03affdd5ee1d19638832a17c
Yash Raj <yashraj.kv@gmail.com>
2025-11-15 11:38:17 +0530

    Navigate playground script

---
+++ LearningPlayground/Navigate.py
@@ -0,0 +1,48 @@
+'''
+Author : Yash Raj
+Created on Nov 14, 2025
+
+Activity:
+a) Opening web page
+b) Navigating web page
+    - Navigate to Google
+    - Navigate to youtube
+    - go back to Google
+    - go forward to YouTube
+    - Refresh the current page (Youtube)
+    - Navigate to Google
+    - Close the brower
+    
+'''
+
+#import webdriver module
+from selenium import webdriver
+import time
+
+#create object of webdriver to launch driver
+driver = webdriver.Chrome()
+#maximize browser
+driver.maximize_window()
+
+google_url = "https://www.google.com"
+youtube_url = "https://www.youtube.com"
+
+#Navigate to Google
+driver.get(google_url)
+#Navigate to youtube
+driver.get(youtube_url)
+#go back to Google
+driver.back()
+#go forward to YouTube
+driver.forward()
+#Refresh the current page (Youtube)
+driver.refresh()
+#Navigate to Google
+driver.back()
+#Close the brower
+driver.quit()
+
+
+
+#close browser
+driver.quit()
