728f6fb45c431a113e8b758be0d9feec2e3d4f7e
Yash Raj <yashraj.kv@gmail.com>
2025-11-17 19:42:48 +0530

    Locator practice with Text, Contains method, Scroll via javascript

--- LearningPlayground/LocatorsPractice.py
+++ LearningPlayground/LocatorsPractice.py
@@ -71,5 +71,58 @@ def fb_forgot_password_page():
     time.sleep(3)
     driver.quit()
 
-fb_forgot_password_page()
+def fb_with_index_locators():
+    driver.get("https://en-gb.facebook.com/")
+    time.sleep(3)
+
+    # finding and clicking in same line
+    driver.find_element(By.PARTIAL_LINK_TEXT, "Forgotten").click()
+
+    #locate username and password using index
+    driver.find_element(By.XPATH,"(//input)[1]").send_keys("test_mail")
+    driver.find_element(By.XPATH,"(//input)[2]").send_keys("test_passsword")
+
+
+
+    time.sleep(3)
+    driver.quit()
+
+def Scroll_Practice():
+    # open url
+    driver.get("https://practice.expandtesting.com/login")
+
+    # locate username and password
+    username = driver.find_element(By.XPATH, "//input[@id ='username']")
+    password = driver.find_element(By.XPATH, "//input[@id ='password']")
+    time.sleep(3)
+    #javascript to execute scroll till element
+    driver.execute_script("arguments[0].scrollIntoView(true);",password)
+
+    # send username and password
+    username.send_keys("practice")
+    password.send_keys("SuperSecretPassword!")
+
+    # locate Login button
+    # login_button = driver.find_element(By.XPATH,"//button[@id ='submit-login']")
+
+    # locate login button using (contains) method
+    # login_button = driver.find_element(By.XPATH, "//button[contains(@class,'btn-primary')]")
+
+    # locate login button using text method
+    login_button = driver.find_element(By.XPATH, "//button[text()='Login']")
+    time.sleep(3)
+    # javascript to execute scroll till element
+    driver.execute_script("arguments[0].scrollIntoView(true);",login_button)
+    # click login button
+    login_button.send_keys(Keys.RETURN)
+
+    time.sleep(3)
+    driver.quit()
+
+
+
+# login_practice()
+# fb_forgot_password_page()
+# fb_with_index_locators()
+Scroll_Practice()
 
