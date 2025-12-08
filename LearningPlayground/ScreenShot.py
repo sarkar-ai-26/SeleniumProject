69277aca8b0dea1ecb577c24704a8735e1f2d810
Yash Raj <yashraj.kv@gmail.com>
2025-12-08 20:20:28 +0530

    IFrames hanled, and Handling of multiple browser windows

--- LearningPlayground/ScreenShot.py
+++ LearningPlayground/ScreenShot.py
@@ -11,7 +11,9 @@ Learning working with Screenshots
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os
+import time
 
 driver = webdriver.Chrome()
 driver.maximize_window()
@@ -23,4 +25,20 @@ def takeScreenshot():
     if not os.path.exists(folder):
         os.mkdir(folder)
 
+
     driver.get("https://practice.expandtesting.com/tooltips")
+
+    file_path = folder + "//file_2.png"
+
+    #full page screenshot
+    # driver.get_screenshot_as_file(file_path)
+    driver.save_screenshot(file_path)
+
+    #element screenshot
+    element_path = folder + "//elementfile.png"
+    button1 = driver.find_element(By.XPATH, "(//button)[1]")
+    time.sleep(3)
+    button1.screenshot(element_path)
+    driver.close()
+
+takeScreenshot()
